272db26f59ae6e0a5e36171e12f5b9fdf73c2e63
salvadorsuz <salvadorsuz@yahoo.es>
2019-05-30 17:31:33 +0200

    initial commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+AppHeader.propTypes = {
+
+};
+
+export default AppHeader;
