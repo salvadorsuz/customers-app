371438ff6cab726bf671bace0e971c482952527c
salvadorsuz <salvadorsuz@yahoo.es>
2019-05-30 20:11:25 +0200

    components

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class AppHeader extends Component {
-    render() {
-        return (
-            <div>
-                
+const AppHeader = ({title}) => {
+    return (
+        <div>
+            <div className="app-header">
+                <h1>{title}</h1>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 AppHeader.propTypes = {
-
+    title: PropTypes.string.isRequired,
 };
 
 export default AppHeader;
